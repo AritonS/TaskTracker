8ec3c6402e7ef7b059d7b5b0373926846f27e0eb
AritonS <aritonsefedini@icloud.com>
2024-05-14 14:25:11 -0400

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Project.find_or_create_by!(title: 'Create App', description: 'Create an app where users can keep track of tasks', status: 'In Progress')
+Todo.find_or_create_by!(title: 'Add Models', description: 'Create models for projects and todos, with todos optionally associated with projects', status: 'In Progress', project_id: '1')
