1af7de8d641eafde08094909bab1848313c72369
AritonS <aritonsefedini@icloud.com>
2024-05-14 14:12:43 -0400

    Created and updated todos_controller.rb

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,19 @@
+class TodosController < ApplicationController
+
+    def index
+        @todos = Todo.all 
+        render json: @todos
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        @todo.save
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:title, :description, :status)
+    end
+
+end
